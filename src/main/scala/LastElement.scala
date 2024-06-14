551ec8b7a513d374e0329e4a7adcc248c47f717a
Jibran Kalia <jibran.kalia@gmail.com>
2024-06-13 19:31:35 -0500

    feat: LastElement

---
+++ src/main/scala/LastElement.scala
@@ -0,0 +1,18 @@
+object LastElement {
+  def lastEasy[A](ls: List[A]): A = ls.last
+
+  def lastRecursive[A](ls: List[A]): A = {
+    println(ls)
+
+    ls match {
+      case h :: Nil => h
+      case _ :: tail => lastRecursive(tail)
+      case _ => throw new NoSuchElementException()
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val testList = List(1, 1, 2, 3, 5)
+    println(s"lastBuiltin(testList): ${lastRecursive(testList)}")
+  }
+}
