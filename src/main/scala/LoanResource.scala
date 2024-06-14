2e33ad337cff7d686e79e0ad01290c6ec62fdad9
Jibran Kalia <jibran.kalia@gmail.com>
2024-06-14 17:48:13 -0500

    feat: LoanResource pattern

---
+++ src/main/scala/LoanResource.scala
@@ -0,0 +1,41 @@
+import java.io._
+
+import java.io._
+
+object LoanResource {
+  def originalReadFile(file: File): Unit = {
+    val reader = new BufferedReader(new FileReader(file))
+    try {
+      var line = reader.readLine()
+      while (line != null) {
+        println(processLine(line))
+        line = reader.readLine()
+      }
+    } finally reader.close()
+  }
+
+  def processLine(line: String): String = {
+    line.toUpperCase()
+  }
+
+  def doEachLine(fileName: String)(op: String => String): Unit = {
+    val file = new File(fileName)
+    val reader = new BufferedReader(new FileReader(file))
+
+    try {
+      var line = reader.readLine()
+      while (line != null) {
+        println(op(line))
+        line = reader.readLine()
+      }
+    } finally {
+      reader.close()
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    doEachLine("example.txt") {
+      line => line.toUpperCase()
+    }
+  }
+}
