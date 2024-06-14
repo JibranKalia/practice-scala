92979f0dd564001af9859ab38bce03d9a62b2190
Jibran Kalia <jibran.kalia@gmail.com>
2024-06-13 19:17:29 -0500

    feat: make multiTable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,21 @@
 object Main extends App {
-  println("Hello, Scala!")
+  def makeRowSeq(row: Int) = {
+    for (col <- 1 to 10 ) yield {
+      val product = (col * row).toString
+      val padding = " " * (4 - product.length)
+      padding + product
+    }
+  }
+
+  def makeRow(row: Int): String = makeRowSeq(row).mkString;
+
+  def multiTable(): Unit = {
+    val tableString =
+      for (row <- 1 to 10)
+        yield makeRow(row)
+
+    println(tableString.mkString("\n"))
+  }
+
+  multiTable()
 }
