ab96861f756bb1996f32ca52e0b9fb37c4eaab4f
Jibran Kalia <jibran.kalia@gmail.com>
2024-07-05 13:13:12 -0500

    retry backoff

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "practice-scala"
+version := "0.1"
+scalaVersion := "2.13.8"
+libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.4.2"
+
+
---
+++ src/main/scala/RetryWithBackoff.scala
@@ -0,0 +1,36 @@
+import scalaj.http._
+
+object RetryWithBackoff {
+  def retry[T](max: Int)(f: => T): T = {
+    var tries = 0
+    var result: Option[T] = None
+    while (result.isEmpty) {
+      try {
+        result = Some(f)
+      } catch {
+        case e: Throwable =>
+          tries += 1
+          if (tries > max) throw e
+          else {
+            println(s"failed, retry #$tries")
+          }
+      }
+    }
+    result.get
+  }
+
+  val httpbin = "https://httpbin.org"
+
+  def main(args: Array[String]): Unit = {
+    retry(max = 5) {
+      // Only succeeds with a 200 response
+      // code 1/3 of the time
+      val response = Http(s"$httpbin/status/200,400,500").asString
+      if (response.is2xx) {
+        println("Success")
+      } else {
+        throw new RuntimeException("Request failed")
+      }
+    }
+  }
+}
